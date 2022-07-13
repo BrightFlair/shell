50e9bc3b8ec82a810a4fda86a9d13cb3db60fda0
Tom Clare <tclare50@gmail.com>
2022-07-13 11:51:16 +0100

    cleaned and commented notification file

--- www/js/index.js
+++ www/js/index.js
@@ -3,36 +3,31 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
-let bgLocationServices;
-
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    //navigator.geolocation.watchPosition(checkPos);
+    //navigator.geolocation.watchPosition(checkPos); // this begins the Roundhouse location functionality
 
+
+    // Configure plugin parameters
     BackgroundGeolocation.configure({
-        HighAccuracy: 1,
-        DistanceFilter: 1,
-        mockLocationsEnabled: true,
-        NotificationTitle: "Background tracking",
+        HighAccuracy: 1, // bool
+        DistanceFilter: 1, // how large is the location "deadzone"
+        stationaryRadius: 1, // What counts as stationary?
+        NotificationTitle: "Background tracking", // Notification that shows while tracking is enabled
         NotificationText: "enabled",
         debug: true,
-        url: "http://localhost/notification.html",
-        postTemplate: {
-            lat: '@latitude',
-            long: '@longitude',
-        }
-
     });
 
-    BackgroundGeolocation.start();
-
+    // When a location change is detected (with respect to DistanceFilter and stationaryRadius)
     BackgroundGeolocation.on('location', function(location) {
-        console.log(location);
-        // handle your locations here
-        // to perform long running operation on iOS
-        // you need to create background task
+        console.log(location); // TODO: remove this - it is for debug purposes
+        checkLocations(location); // check update location against stored trigger locations
+
+
+        // to perform long running operation on iOS with this plugn...
+        // ...you need to create background task
         //BackgroundGeolocation.startTask(function(taskKey) {
           // execute long running task
           // eg. ajax post location
@@ -43,8 +38,7 @@ function onDeviceReady() {
 
 }
 
-
-
+// checks if pos is outside the Roundhouse and calls visual feedback function
 function checkPos(pos) {
     const crd = pos.coords;
 
@@ -59,6 +53,7 @@ function checkPos(pos) {
     setRoundHouse(distance <= radius);
 }
 
+// returns distance between two coordinates in degrees
 function getEuclidDistance(current, target) {
     let lat_distance  = Math.abs(current.latitude - target.latitude);
     let long_distance = Math.abs(current.longitude - target.longitude);
@@ -67,11 +62,7 @@ function getEuclidDistance(current, target) {
     return distance;
 }
 
-function notificationDismissed() {
-    // nothing yet
-}
-
-
+// Demonstrates locally-managed foreground geolocation feedback
 function setRoundHouse(areWeThere) {
     let form = document.querySelector("body>form");
     if(areWeThere) {
@@ -82,21 +73,17 @@ function setRoundHouse(areWeThere) {
     }
 }
 
-function checkLocations(pos) {
-    const crd = pos.coords;
-
+function checkLocations(position) {
     // we may have a long list of registered location triggers, each of a different type.
-    // For now, let's assume there is a single registered location trigger (they only have one concert that day)
-    var boundaries = globalThis.data.payload.boundaries;
-    const length = Object.keys(boundaries).length;
+    var boundaries = globalThis.data.payload.boundaries; // retreieve all trigger boundaries
+    const length = Object.keys(boundaries).length; // get total amount of boundaries
 
     let distance;
-    for (var i = 0; i < length; i++) {
+    for (var i = 0; i < length; i++) { // for each trigger boundary...
         // check geofence
-        var trigger = boundaries[i];
-        distance = getEuclidDistance(crd, trigger);
-        if (distance < trigger.radius) {
-            cordova.plugins.notification.local.schedule({
+        distance = getEuclidDistance(position, boundaries[i]);
+        if (distance < boundaries[i].radius) { // if inside radius
+            cordova.plugins.notification.local.schedule({ // create notification
                 title: globalThis.data.payload.title,
                 text: globalThis.data.payload.subtitle,
                 foreground: true
@@ -108,28 +95,30 @@ function checkLocations(pos) {
 
 // Listen to message from child window
 window.addEventListener('message', e => {
-    var key = e.message ? "message" : "data";
+    var key = e.message ? "message" : "data"; // find out if posteMessage is message or data
     var data = e[key];
     var origin = e.origin;
 
     // Need to assess origin rule out cross-site scripting attacks
-    if (origin !== "http://localhost:8080")  { /////////////////// replace with expected origin point (pwa.g105b.com)
+    if (origin !== "http://localhost:8080")  { // TODO: replace with expected origin point (pwa.g105b.com)
         return;
     }
 
+    // what to do with different message types
     if (data.type == "notification") {
-        if (data.subtype == "now") {
+        if (data.subtype == "now") { // Create a notification right now
             cordova.plugins.notification.local.schedule({
                 title: data.title,
                 text: data.subtitle,
                 foreground: true
             });
-        } else if (data.subtype == "geofence") {
-            globalThis.data = data;
+        } else if (data.subtype == "geofence") { // create a notification to be triggered when the user enters a physical area
+            globalThis.data = data; // save notification data in globally accessible place
+            BackgroundGeolocation.start(); // begin location checking
             // when location updates
                 // location check
                     // notification
-        } else if (data.subtype == "timed") {
+        } else if (data.subtype == "timed") { // schedule a notification for a spcific time
             cordova.plugins.notification.local.schedule({
                 title: data.payload.title,
                 text: data.payload.subtitle,
@@ -138,13 +127,14 @@ window.addEventListener('message', e => {
                     data.payload.month,
                     data.payload.day,
                     data.payload.hour,
-                    data.payload.minute
+                    data.payload.minute,
+                    //data.payload.second
                 )}
             });
         }
-    } else if (data.type == "debug") {
+    } else if (data.type == "debug") { // debugging features
         if (data.subtype == "geolocation") {
-            navigator.geolocation.getCurrentPosition(PositionSuccess, PositionError, {timeout: 5000});
+            //navigator.geolocation.getCurrentPosition(PositionSuccess, PositionError, {timeout: 5000});
         }
     }
 });
